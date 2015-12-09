e9f4a5bc1a3e1b0affe6b0505def7bbcd4b0ccfb
Accelll <owenshotbolt@gmail.com>
2015-12-09 11:21:41 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -79,7 +79,8 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+&& $imageFileType != "gif" && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG"
+&& $imageFileType != "GIF") {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
